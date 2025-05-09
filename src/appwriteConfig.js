34c308f5ce609011bad4817dbd475aeb07ee0c0e
AdyaKumarjha <adyakumar8434@gmail.com>
2025-05-09 11:41:07 +0530

    customer login page

---
+++ src/appwriteConfig.js
@@ -0,0 +1,10 @@
+import { Client, Account } from 'appwrite';
+
+const client = new Client();
+client
+    .setEndpoint('https://fra.cloud.appwrite.io/v1')
+    .setProject('681d7f11001705ba7a8b');
+
+    export const account = new Account(client);
+
+    export default client;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+  
+        <div className="container">
+            <h1>Welcome to my website!</h1>
+
+            <img src="https://images.pexels.com/photos/7464677/pexels-photo-7464677.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        </div>
+
+  )
+}
+
+export default Home
